ae7f9def90b734612174760ca7a8c1e4e8d63e2e
Gerzhan <nikolay.gerzhan@gmail.com>
2023-05-30 16:07:04 +0700

    fix: fix svelte.config.js for build

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		// @see https://github.com/michmich112/sveltekit-adapter-chrome-extension
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			manifest: 'manifest.json'
+		}),
+		appDir: 'app'
 	}
 };
 
