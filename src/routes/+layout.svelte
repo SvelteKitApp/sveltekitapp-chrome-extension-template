553cba400f9f45215b9897b30b91d838d43dddeb
Gerzhan <nikolay.gerzhan@gmail.com>
2023-05-30 17:34:52 +0700

    feat: add environment detection

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	// Your selected Skeleton theme:
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 
@@ -7,6 +7,41 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Finally, your application's global stylesheet (sometimes labeled 'app.css')
 	import '../app.postcss';
+	import { onMount } from 'svelte';
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+
+	let environment: string;
+
+	onMount(() => {
+		// NOTE: определение окружения выполнения кода
+		if (window.chrome && chrome.runtime && chrome.runtime.id) {
+			environment = 'Chrome Extension';
+		} else {
+			environment = 'Development';
+		}
+	});
 </script>
 
-<slot />
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">SvelteKitApp | {environment}</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/SvelteKitApp"
+					target="_blank"
+					rel="noreferrer"
+				>
+					SvelteKitApp
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
